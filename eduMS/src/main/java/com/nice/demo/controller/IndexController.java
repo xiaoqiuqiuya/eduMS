90d735b01560fca005649dabf73c48cb874e1c93
nice-paiqiu <1442722749@qq.com>
2020-02-29 00:19:45 +0800

    render the myClass table

--- eduMS/src/main/java/com/nice/demo/controller/IndexController.java
+++ eduMS/src/main/java/com/nice/demo/controller/IndexController.java
@@ -18,14 +18,14 @@ public class IndexController {
     @GetMapping("/index")
     public String index(HttpServletRequest request, Model model) {
         //判断是否通过修改地址栏直接进入index页面--查看session
-        Teacher teacher = (Teacher) request.getSession().getAttribute("teacher");
-        if( teacher==null){
-            System.out.println("通过地址栏进来，并没有进行登录");
-
-            return "login";
-        }
+//        Teacher teacher = (Teacher) request.getSession().getAttribute("teacher");
+//        if( teacher==null){
+//            System.out.println("通过地址栏进来，并没有进行登录");
+//
+//            return "login";
+//        }
 //        获取名字
-        model.addAttribute("name",teacher.getName());
+//        model.addAttribute("name",teacher.getName());
         //返回 templates中 同名为index的文件
         System.out.println("进入index");
         return "index";
